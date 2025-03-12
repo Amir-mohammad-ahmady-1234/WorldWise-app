9167e786fd6d324913618bb5eb5eb800345c8d1c
amirMohammad <amyrmhmdahmdy179@gmail.com>
2025-03-12 16:00:36 -0700

    Building the App Layout ( Static part of the project )

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -17,7 +17,7 @@ export default function Homepage() {
           of. Never forget your wonderful experiences, and show your friends how
           you have wandered the world.
         </h2>
-        <Link className="cta" to='.app'>START TRACKING NOW</Link>
+        <Link className="cta" to='/app'>START TRACKING NOW</Link>
       </section>
     </main>
   );
