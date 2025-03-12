ac08430cf7e9088c922abd2a4d6ade81d5469e3a
amirMohammad <amyrmhmdahmdy179@gmail.com>
2025-03-11 20:04:13 -0700

    using css module in prohect and added app page

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,17 @@
+import { Link } from "react-router-dom";
+import AppNav from "../components/AppNav";
 import PageNav from "../components/PageNav";
 
 function HomePage() {
   return (
     <div>
       <PageNav />
+      <AppNav />
+
+
       <h1>WorldWise</h1>
+
+      <Link to='/app'>go to App</Link>
     </div>
   );
 }
